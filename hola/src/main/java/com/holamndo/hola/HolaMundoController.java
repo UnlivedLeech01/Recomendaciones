b27b6df2537284e1ffd8f4124fc2b60752a5ef73
UnlivedLeech01 <134242581+UnlivedLeech01@users.noreply.github.com>
2025-02-19 22:28:39 -0600

    Add files via upload

---
+++ hola/src/main/java/com/holamndo/hola/HolaMundoController.java
@@ -0,0 +1,16 @@
+package com.holamndo.hola;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HolaMundoController {
+    
+    @GetMapping("/hola")
+    public String decirHola() {
+
+        return "Hola Spring";
+    }
+
+}
